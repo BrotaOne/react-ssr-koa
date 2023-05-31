f3640303474ca456ec44d02bdf718096790c1bf7
gongping01 <gongping01@baidu.com>
2023-05-31 16:23:00 +0800

    feat: 更新 README、App.tsx

---
+++ README.md
@@ -0,0 +1,35 @@
+# React-SSR-koa
+
+## 一、React - SSR
+
+### 1、什么是 SSR
+
+### 2、官方说明
+
+### 3、使用吃瘪
+
+结论：不会用 koa
+
+## 二、koa 搭建服务器
+
+### 三、React-SSR-koa
+
+## 四、其他
+
+遇到的问题：
+
+1、node 中使用 import、jsx
+
+不要写 tsx 后缀
+
+2、`*renderToString*(*ServerApp*())` hook 无法使用
+
+要先编译后使用
+
+
+
+使用
+
+1. 运行 `pnpm build` 编译客户端和服务端代码
+2. 运行 `pnpm server` 启动 koa 服务
+3. 打开 localhost:3000 查看效果
--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,24 @@
-import React, {useState} from 'react';
+import React, { FC, useState, useEffect } from 'react';
+
+const Child: FC<{ count: number }> = ({ count }) => {
+    useEffect(
+        () => {
+            console.log('mount');
+
+            return () => {
+                console.log('unmount');
+            }
+        },[]
+    )
+    return <div className="child">Count: {count}</div>
+}
 
 export default function App() {
     const [count, setCount] = useState(0);
     const add = () => setCount(v => v + 1);
     return <div>
         <div>App</div>
-        <div>Count: {count}</div>
+        <Child  count={count} />
         <button onClick={add}>+1</button>
     </div>;
 }
